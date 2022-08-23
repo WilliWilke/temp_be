48e869d3b27c1dd9f93b4cac868fd8f7868292d0
Kevin Bulteel <kb@koalasoft.de>
2022-08-23 14:24:49 +0200

    task2 ready

--- task_2/bin/csv2json.mjs
+++ task_2/bin/csv2json.mjs
@@ -1,7 +1,43 @@
 export default function csv2json(csv) {
-  let json = [];
+  let json    = [],
+      lines   = csv.split('\n'),
+      headers = lines[0].split(';');
 
-  //???
+  for (let i = 1; i < lines.length; i++) {
+    let line = lines[i], obj = {}, values = [];
 
-  return json;
+    for (let j = 0, c, inQuotes = false, str = ''; j < line.length; j++) {
+      c = line[j];
+
+      if (c == ';' && !inQuotes || j == line.length - 1) {
+        values.push(str);
+
+        str = ''; 
+        
+        continue;
+      } else if (c == '\'') {
+        inQuotes = !inQuotes;
+
+        continue;
+      } else if (c == '\\') {
+        j++; 
+
+        str += line[j];
+        
+        continue;
+      }
+
+      str += c;
+
+    }
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = values[j];
+    }
+
+    json.push(obj);
+
+  }
+
+  return JSON.stringify(json, null, 2);
 }
